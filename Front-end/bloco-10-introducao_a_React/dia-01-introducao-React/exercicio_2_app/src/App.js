4b5e871dd13076a0df90a1433a385363d0d17d38
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-06-10 21:53:20 -0300

    questão 3

---
+++ Front-end/bloco-10-introducao_a_React/dia-01-introducao-React/exercicio_2_app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Hearder from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <>
+    <Hearder />
+    <Content />    
+    <Footer />
+
+    </>
+  );
+}
+
+export default App;
